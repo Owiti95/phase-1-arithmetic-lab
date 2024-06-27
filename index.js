a3cea2747660e8147945d88d958be124212b90ac
hannington owiti <hanningtonowiti@gmail.com>
2024-06-28 00:03:59 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//random integer greater than 0
+let random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+console.log(random);
+
+//remainder
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+
+console.log(mod);
+
+//max
+let set = [2, 4, 20, 8, 10];
+let max = Math.max(...set);
+
+console.log(max);
